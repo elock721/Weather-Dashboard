931c46c5ebb82116e9687bfd02116e172d1588a4
Elliott Lockwood <lockwood721@gmail.com>
2020-06-30 00:21:40 -0500

    Working draft

--- README.md
+++ README.md
@@ -1,46 +1,29 @@
-# 06 Server-Side APIs: Weather Dashboard
+Weather Dashboard Overview
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+This weather dashboard application allows you to search for a city's current weather
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+When you search for your desired city, the following weather characteristics will be returned - temperature (fahrenheit), humidity, wind speed, and UV index
 
-## User Story
+A 5 day forecast will populate as well with the date's above and the temperature (fahrenheit)/humidity
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+As soon as all the weather information about your searched city populates, it will be logged directly below the search bar 
 
-## Acceptance Criteria
+The table below will log the 8 most recent searches only
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-WHEN I open the weather dashboard
-THEN I am presented with the last searched city forecast
-```
+All of the individual cities in the history table below the search bar also become clickable shortcut links to display that certain city's current weather
 
-The following image demonstrates the application functionality:
+------------------------------------------------
 
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
+Psuedo Code
 
-## Review
+When the user enters a citys and clicks search, the searchWeather function is connected to an API call from openweathermap.org that retrieves the city name, temperature (fahrenheit), humidity, and wind speed.  
 
-You are required to submit the following for review:
+The geUvIndex function below it is created specifically for the UV Index
 
-* The URL of the deployed application.
+The fiveDay function is simultaneously to populate the 5 day forcast of the searched city complete with temperature (fahrenheit) and humidity as well as the date on top
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+All cards for the 5 day function are created with jQuery 
 
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+The saveSearch function then logs it in local storage 
+
+And finally the populateSearch function adds the last city search query to the history below the search bar as a clickable shortcut for the city's weather
--- script.js
+++ script.js
@@ -1,94 +1,137 @@
-$(document).ready(function(){
-    $("#searchBtn").on("click", function() {
+$(document).ready(function () {
+    // grabs text value from search bar
+    $("#searchBtn").on("click", function () {
         var searchValue = $("#searchText").val();
 
         $("#searchText").val("");
         console.log(searchValue);
-    
+
         searchWeather(searchValue);
         fiveDay(searchValue);
     });
-    
+
+    // returns city search query to search history below search bar
     $(".history").on("click", "li", function () {
         var city = $(this).text()
         searchWeather(city);
         fiveDay(searchValue);
     })
+    loadHistory();
 
 });
 
-function searchWeather(searchText) {
+// data exchange with the web server 
+function loadHistory() {
+    let searchedCities = localStorage.getItem("weatherApp")
+    if (searchedCities) {
+        populateSearch(JSON.parse(searchedCities))
+    }
+}
+
+// function to get city and weather information
+function searchWeather(searchText, savedSearch = false) {
     console.log(searchText)
     $.ajax({
         type: "GET",
+        // API call for all city and weather info expect for UV index
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=e62facd2394fa5ea9004691f3f101acd&units=imperial",
-        dataType: "json", 
-        success: function(data) {
-            console.log(data)
+        dataType: "json",
+        success: function (data) {
+            $("#current-city").text(data.name)
             $("#temp1").text(data.main.temp + " F")
             $("#hum1").text(data.main.humidity + "%")
             $("#wind1").text(data.wind.speed + " MPH")
             var coords = data.coord;
-            getUvIndex(coords.lat, coords.lon)    
-        }, 
-        error: function(error){
+            getUvIndex(coords.lat, coords.lon)
+            if (!savedSearch) {
+                saveSearch(data.name)
+            }
+
+        },
+        error: function (error) {
             console.log(error);
         }
-        
-
 
     })
-    
-}
 
-function getUvIndex (lat, long) {
+}
+// function to get UV index
+function getUvIndex(lat, long) {
     console.log(lat, long)
     $.ajax({
         type: "GET",
-        url: "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=e62facd2394fa5ea9004691f3f101acd&lat="+lat+"&lon="+long+"&cnt=1",
-        dataType: "json", 
-        success: function(data) {
+        // API call for UV index
+        url: "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=e62facd2394fa5ea9004691f3f101acd&lat=" + lat + "&lon=" + long + "&cnt=1",
+        dataType: "json",
+        success: function (data) {
             console.log(data[0].value)
             $("#ind1").text(data[0].value)
-        }, 
-        error: function(error){
+        },
+        error: function (error) {
             console.log(error);
         }
-       
-
 
     })
 }
-
-function fiveDay (searchText){
+// function to return 5 day forecast
+function fiveDay(searchText) {
     $.ajax({
         type: "GET",
-        url: "http://api.openweathermap.org/data/2.5/forecast?q= "+searchText+" &appid=e62facd2394fa5ea9004691f3f101acd&units=imperial",
-        dataType: "json", 
-        success: function(data) {
-            // console.log(data)
-        for (let i = 0; i < data.list.length; i++) {
-            const element = data.list[i];
-            if(element.dt_txt.indexOf("15:00:00") !== -1){
-                console.log(element);
-                var card = $("<div>").addClass("card border-dark mb-3")
-                var cardHeader = $("<div>").addClass("card-header")
-                var cardBody = $("<div>").addClass("card-body text-dark")
-                var temp = $("<p>").addClass("card-text").text(element.main.temp)
-                cardBody.append(temp)
-                card.append(cardHeader, cardBody)
-                $(".wrapper3").append(card);
+        url: "http://api.openweathermap.org/data/2.5/forecast?q= " + searchText + " &appid=e62facd2394fa5ea9004691f3f101acd&units=imperial",
+        dataType: "json",
+        success: function (data) {
+            console.log(data)
+            $(".wrapper3").empty();
+            for (let i = 0; i < data.list.length; i++) {
+                const element = data.list[i];
+                if (element.dt_txt.indexOf("15:00:00") !== -1) {
+                    console.log(element);
+                    var card = $("<div>").addClass("card border-dark mb-3")
+                    var cardHeader = $("<div>").addClass("card-header")
+                    var cardBody = $("<div>").addClass("card-body text-dark")
+                    var temp = $("<p>").addClass("card-text").text("Temp: " + element.main.temp + " F")
+                    var humi = $("<p>").addClass("card-text").text("Humidity: " + element.main.humidity + "%")
+                    var date = $("<p>").addClass("card-text").text(element.dt_txt.split(" ")[0])
+                    cardHeader.append(date)
+                    cardBody.append(temp)
+                    cardBody.append(humi)
+                    card.append(cardHeader, cardBody)
+                    $(".wrapper3").append(card);
+                }
+
             }
-            
-        }
-        }, 
-        error: function(error){
+        },
+        error: function (error) {
             console.log(error);
         }
-       
-
 
     })
 
 }
+// logs searches in local storage
+function saveSearch(city) {
+    let searchedCities = localStorage.getItem("weatherApp");
+    if (!searchedCities) {
+        searchedCities = []
+    } else {
+        searchedCities = JSON.parse(searchedCities)
+    }
+    searchedCities.unshift(city)
+    localStorage.setItem("weatherApp", JSON.stringify(searchedCities.slice(0, 7)))
+    populateSearch(searchedCities);
+}
 
+// function to display search history below search bar
+function populateSearch(cities) {
+    $("#searchHistory").empty()
+    for (let i = 0; i < cities.length; i++) {
+        const element = cities[i];
+        let li = $("<li>").addClass("list-group-item").text(element)
+        $(li).on("click", function () {
+            searchWeather(element, true);
+            fiveDay(element);
+        })
+        $("#searchHistory").append(li)
+
+    }
+}
